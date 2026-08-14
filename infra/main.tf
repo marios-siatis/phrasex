75bc2f95dde5ecdbd2eed17a5e32f18335374f6d
Marios <marios.siati@outlook.com>
2026-08-14 18:46:17 +0100

    frontend access denied issue first deployment

--- infra/main.tf
+++ infra/main.tf
@@ -177,6 +177,37 @@ resource "aws_cloudfront_distribution" "web" {
   }
 }
 
+resource "aws_s3_bucket_policy" "web" {
+  bucket = aws_s3_bucket.web.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontRead"
+        Effect = "Allow"
+
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+
+        Action = [
+          "s3:GetObject"
+        ]
+
+        Resource = "${aws_s3_bucket.web.arn}/*"
+
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.web.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
 # ---------------------------------------------------------
 # CloudWatch
 # ---------------------------------------------------------
