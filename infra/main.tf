5c90fe6b3d4e564887699a7d4775b12dd9ed40dd
Marios <marios.siati@outlook.com>
2026-08-15 00:14:32 +0100

    adds https api certifcate

--- infra/main.tf
+++ infra/main.tf
@@ -128,6 +128,13 @@ resource "aws_security_group" "alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -439,6 +446,26 @@ resource "aws_lb_listener" "api" {
   protocol          = "HTTP"
 
   default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "api_https" {
+  load_balancer_arn = aws_lb.api.arn
+  port              = 443
+  protocol          = "HTTPS"
+
+  ssl_policy = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+
+  certificate_arn = aws_acm_certificate.api.arn
+
+  default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.api.arn
   }
@@ -542,6 +569,7 @@ resource "aws_ecs_service" "api" {
 
   depends_on = [
     aws_lb_listener.api,
+    aws_lb_listener.api_https,
     aws_db_instance.postgres
   ]
 }
